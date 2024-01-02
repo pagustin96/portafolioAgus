fdc799d2f770cc6ced5c4bf98467e18d720577b8
Agustin Sanchez <pagustinsanchez17@gmail.com>
2024-01-02 18:43:57 -0300

    actualizacionV2

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -84,7 +84,7 @@ export const About = () => {
                         Mi enfoque principal se encuentra en el frontend, donde me especializo en la creación 
                         de experiencias de usuario atractivas y eficientes, combinando el diseño y 
                         la programación para ofrecer soluciones innovadoras.</p>
-                        <p>Javascript - React Js - Java - SpringBoot - MySql</p>
+                        <p>Javascript - React Js - Java - SpringBoot - MySQL</p>
                     </div>
                 </div>
                 <div className='experience-box'>
@@ -96,7 +96,7 @@ export const About = () => {
                         Odoo, colaborando estrechamente con el equipo de analistas. Mi rol consistió en el desarrollo,
                          modificación, mantenimiento e implementación de software de gestión, asegurando la eficacia
                           y la optimización de los procesos empresariales.</p>
-                        <p>Javascript - React - Node Js - Express Js - MySql</p>
+                        <p>Javascript - React - Node Js - Express Js - MySQL</p>
                     </div>
                 </div>
             </section>
@@ -199,7 +199,7 @@ export const About = () => {
                 <p className='from'>from</p> 
                 <h3 className='pais'>Argentina</h3>
                 <p className='footer-p'>
-                Hecho por Pedro Agustin Sanchez con <strong>React</strong>, Vite, Javascript, HTML, Css y mucho <div className='love'><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="#e2dfdf" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></div>.
+                Hecho por Pedro Agustin Sanchez con <strong>React</strong>, Vite, Javascript, HTML, CSS y mucho <div className='love'><svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path fill-rule="evenodd" clip-rule="evenodd" d="M12 6.00019C10.2006 3.90317 7.19377 3.2551 4.93923 5.17534C2.68468 7.09558 2.36727 10.3061 4.13778 12.5772C5.60984 14.4654 10.0648 18.4479 11.5249 19.7369C11.6882 19.8811 11.7699 19.9532 11.8652 19.9815C11.9483 20.0062 12.0393 20.0062 12.1225 19.9815C12.2178 19.9532 12.2994 19.8811 12.4628 19.7369C13.9229 18.4479 18.3778 14.4654 19.8499 12.5772C21.6204 10.3061 21.3417 7.07538 19.0484 5.17534C16.7551 3.2753 13.7994 3.90317 12 6.00019Z" stroke="#e2dfdf" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></div>.
                  Desplegado en 
                 GitHub Pages. <br/>
                 {`"`}Si se puede <strong>imaginar</strong> se puede <strong>programar</strong>{`"`}.
